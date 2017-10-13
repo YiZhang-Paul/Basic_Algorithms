46793a3353a0bb875d33178b625aa281807e89f0
YI ZHANG <scaccterran@gmail.com>
2017-10-13 03:07:33 -0400

    selection sort recursive

--- sorting/selection_sort_recursive/selection_sort_recursive/SelectionSortRecursive.cs
+++ sorting/selection_sort_recursive/selection_sort_recursive/SelectionSortRecursive.cs
@@ -6,6 +6,30 @@ using System.Threading.Tasks;
 
 namespace selection_sort_recursive {
     class SelectionSortRecursive {
-
+        /**
+         * sort an array in ascending order - this algorithm modifies the original array
+         * @param {int[]} [array] - array to sort
+         * @param {int} [visit] - current number of visits to array
+         * 
+         * @return {int[]} [sorted array]
+         */
+        public int[] Sort(int[] array, int visit = 0) {
+            if(visit == array.Length) {
+                return array;
+            }
+            //find minimum value and swap to beginning of array
+            int minIndex = visit;
+            int min = array[minIndex];
+            for(int i = minIndex + 1; i < array.Length; i++) {
+                if(array[i] < min) {
+                    min = array[i];
+                    minIndex = i;
+                }
+            }
+            //swap elements
+            array[minIndex] = array[visit];
+            array[visit] = min;
+            return Sort(array, visit + 1);
+        }
     }
 }
