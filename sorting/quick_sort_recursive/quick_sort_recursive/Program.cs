f37ad9d3de9e137fc94c070026d5fa250271cea4
YI ZHANG <scaccterran@gmail.com>
2017-10-13 14:53:33 -0400

    quick sort recursive

---
+++ sorting/quick_sort_recursive/quick_sort_recursive/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quick_sort_recursive {
+    class Program {
+        static void Main(string[] args) {
+            int[] array = new int[] { 2, 14, 11, 16, 82, 4, 34, 51, 1, 4 };
+            QuickSortRecursive sorter = new QuickSortRecursive();
+            sorter.Sort(array, 0, array.Length - 1);
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
---
+++ sorting/quick_sort_recursive/quick_sort_recursive/QuickSortRecursive.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quick_sort_recursive {
+    class QuickSortRecursive {
+        /**
+         * sort an array in ascending order - this algorithm modifies the original array
+         * @param {int[]} [array] - array to sort
+         * @param {int} [beginIndex] - beginIndex index of partition
+         * @param {int} [endIndex] - endIndex index of partition 
+         */
+        public void Sort(int[] array, int beginIndex, int endIndex) { 
+            if(beginIndex < endIndex) {
+                int pivot = array[endIndex];
+                int startIndex = beginIndex;
+                for(int i = beginIndex; i < endIndex; i++) {
+                    if(array[i] < pivot) {
+                        //swap elements
+                        int tempValue = array[i];
+                        array[i] = array[startIndex];
+                        array[startIndex] = tempValue;
+                        startIndex++;
+                    }
+                }
+                //swap pivot to correct position
+                array[endIndex] = array[startIndex];
+                array[startIndex] = pivot; 
+                //sort partitions
+                Sort(array, beginIndex, startIndex - 1);
+                Sort(array, startIndex + 1, endIndex);
+            }
+        }
+    }
+}
