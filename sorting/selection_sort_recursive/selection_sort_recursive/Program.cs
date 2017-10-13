8ed80cd3124426b4ec2c6c45e9643e23eee09de7
YI ZHANG <scaccterran@gmail.com>
2017-10-13 02:52:40 -0400

    selection sort recursive

---
+++ sorting/selection_sort_recursive/selection_sort_recursive/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selection_sort_recursive {
+    class Program {
+        static void Main(string[] args) {
+            int[] array = new int[] { 2, 14, 11, 16, 82, 4, 34, 51, 1, 4 };
+            SelectionSortRecursive sorter = new SelectionSortRecursive();
+            Console.WriteLine(string.Join(", ", sorter.Sort(array)));
+        }
+    }
+}
