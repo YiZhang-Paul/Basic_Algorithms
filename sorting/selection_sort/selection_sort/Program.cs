0748f5f75c22b4aed2de1e7bd30bd7eb4048b349
YI ZHANG <scaccterran@gmail.com>
2017-10-13 02:47:11 -0400

    selection sort

--- sorting/selection_sort/selection_sort/Program.cs
+++ sorting/selection_sort/selection_sort/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace selection_sort {
     class Program {
         static void Main(string[] args) {
+            int[] array = new int[] { 2, 14, 11, 16, 82, 4, 34, 51, 1, 4 };
+            SelectionSort sorter = new SelectionSort();
+            Console.WriteLine(string.Join(", ", sorter.Sort(array)));
         }
     }
 }
---
+++ sorting/selection_sort/selection_sort/SelectionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selection_sort {
+    class SelectionSort {
+        /**
+         * sort an array in ascending order
+         * @param {int[]} [array] - array to sort
+         * 
+         * @return {int[]} [array]
+         */
+        public int[] Sort(int[] array) {
+            var unsorted = new List<int>(array);
+            var sorted = new List<int>(array.Length);
+            while(unsorted.Count > 0) {
+                //or simply: int min = unsorted.Min();
+                int min = unsorted[0];
+                foreach(int number in unsorted) {
+                    //or simply: min = Math.Min(min, number);
+                    min = number < min ? number : min;
+                }
+                unsorted.Remove(min);
+                sorted.Add(min);
+            }
+            return sorted.ToArray();
+        }
+    }
+}
