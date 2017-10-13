8500bfb7b5ddf2825bdb1d25c351ef8fb557c232
YI ZHANG <scaccterran@gmail.com>
2017-10-13 03:12:30 -0400

    insertion sort

---
+++ sorting/insertion_sort/insertion_sort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insertion_sort {
+    class Program {
+        static void Main(string[] args) {
+            int[] array = new int[] { 2, 14, 11, 16, 82, 4, 34, 51, 1, 4 };
+            InsertionSort sorter = new InsertionSort();
+            Console.WriteLine(string.Join(", ", sorter.Sort(array)));
+        }
+    }
+}
