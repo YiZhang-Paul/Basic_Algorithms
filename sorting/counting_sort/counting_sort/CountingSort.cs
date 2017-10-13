2d56627979cf6932031c7fb1dc4cdb6f0171cbee
YI ZHANG <scaccterran@gmail.com>
2017-10-13 15:22:12 -0400

    counting sort

---
+++ sorting/counting_sort/counting_sort/CountingSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace counting_sort {
+    class CountingSort {
+        /**
+         * sort an array in ascending order
+         * @param {int[]} [array] - array to sort
+         * 
+         * @return {int[]} [sorted array]
+         */
+        public int[] Sort(int[] array) {
+            //counter filled with 0
+            int[] counter = new int[array.Max() + 1];
+            Array.Clear(counter, 0, counter.Length);
+            //count occurrence of numbers and sum up occurrences
+            foreach(int number in array) {
+                counter[number]++;
+            }
+            for(int i = 1; i < counter.Length; i++) {
+                counter[i] += counter[i - 1];
+            }
+            //sort array
+            int[] sorted = new int[array.Length];
+            foreach(int number in array) {
+                sorted[counter[number] - 1] = number;
+                counter[number]--;
+            }
+            return sorted;
+        }
+    }
+}
---
+++ sorting/counting_sort/counting_sort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace counting_sort {
+    class Program {
+        static void Main(string[] args) {
+            int[] array = new int[] { 2, 14, 11, 16, 82, 4, 34, 51, 1, 4 };
+            CountingSort sorter = new CountingSort();
+            Console.WriteLine(string.Join(", ", sorter.Sort(array)));
+        }
+    }
+}
