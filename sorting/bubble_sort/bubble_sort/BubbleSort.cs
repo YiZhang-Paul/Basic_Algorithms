73119ed25a32b0abf8f106b17e85c86c88b05aac
YI ZHANG <scaccterran@gmail.com>
2017-10-13 01:53:04 -0400

    bubble sort

---
+++ sorting/bubble_sort/bubble_sort/BubbleSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubble_sort {
+    class BubbleSort {
+        /**
+         * sort array in ascending order
+         * @param {int[]} [array] - array to sort
+         * 
+         * @return {int[]} [sorted array]
+         */
+        public int[] Sort(int[] array) {
+            for(int i = 0; i < array.Length - 1; i++) {
+                bool swapped = false;
+                /**
+                 * other than swapping unordered pairs, also makes sure to move current max number 
+                 * to the end of array, right before the previous max number; thus the algorithm 
+                 * checks numbers only up to (array.Length - 1 - i) upon each inner loop, since 
+                 * all numbers after that index are larger than the rest and are sorted
+                 */
+                for(int j = 0; j < array.Length - 1 - i; j++) {
+                    if(array[j] > array[j + 1]) {
+                        //swap elements
+                        array[j] += array[j + 1];
+                        array[j + 1] = array[j] - array[j + 1];
+                        array[j] -= array[j + 1];
+                        swapped = true;
+                    }
+                }
+                //optimization; stop when no more swap occur
+                if(!swapped) {
+                    break;
+                }
+            }
+            return array;
+        }
+    }
+}
--- sorting/bubble_sort/bubble_sort/Program.cs
+++ sorting/bubble_sort/bubble_sort/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace bubble_sort {
     class Program {
         static void Main(string[] args) {
+            int[] array = new int[] { 2, 14, 11, 16, 82, 4, 34, 51, 1, 4 };
+            BubbleSort sorter = new BubbleSort();
+            Console.WriteLine(string.Join(", ", sorter.Sort(array)));
         }
     }
 }
