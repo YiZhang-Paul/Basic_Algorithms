066ce4ff6bcad383a5bdd03b8fa50c6cb773b27f
YI ZHANG <scaccterran@gmail.com>
2017-10-13 04:05:09 -0400

    merge sort recursive

---
+++ sorting/merge_sort_recursive/merge_sort_recursive/MergeSortRecursive.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace merge_sort_recursive {
+    class MergeSortRecursive {
+
+    }
+}
---
+++ sorting/merge_sort_recursive/merge_sort_recursive/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace merge_sort_recursive {
+    class Program {
+        static void Main(string[] args) {
+            int[] array = new int[] { 2, 14, 11, 16, 82, 4, 34, 51, 1, 4 };
+            MergeSortRecursive sorter = new MergeSortRecursive();
+            Console.WriteLine(string.Join(", ", sorter.Sort(array)));
+        }
+    }
+}
