67aa9f29dd5976189971e6846ee89902c259a5f3
YI ZHANG <scaccterran@gmail.com>
2017-10-13 03:36:01 -0400

    insertion sort

--- sorting/insertion_sort/insertion_sort/InsertionSort.cs
+++ sorting/insertion_sort/insertion_sort/InsertionSort.cs
@@ -6,6 +6,25 @@ using System.Threading.Tasks;
 
 namespace insertion_sort {
     class InsertionSort {
-
+        /**
+         * sort an array in ascending order - this algorithm modifies the original array
+         * @param {int[]} [array] - array to sort
+         * 
+         * @return {int[]} [sorted array]
+         */
+        public int[] Sort(int[] array) {
+            for(int i = 0; i < array.Length; i++) {
+                int curIndex = i;
+                int curValue = array[curIndex];
+                //keep shifting larger numbers towards original index of current inspecting value
+                while(curIndex > 0 && array[curIndex - 1] > curValue) {
+                    array[curIndex] = array[curIndex - 1];
+                    curIndex--;
+                }
+                //insert original value at appropriate index
+                array[curIndex] = curValue;
+            }
+            return array;
+        }
     }
 }
