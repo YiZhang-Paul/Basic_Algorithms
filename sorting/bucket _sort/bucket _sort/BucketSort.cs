b881d7f505b8ead2e836f374b1cc5743b872a3bd
YI ZHANG <scaccterran@gmail.com>
2017-10-14 16:57:25 -0400

    bucket sort

---
+++ sorting/bucket _sort/bucket _sort/BucketSort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bucket__sort {
+    class BucketSort {
+        /*
+         * sort an array in ascending order - this algorithm modifies the original array
+         * @param {int[]} [array] - array to sort
+         */
+        public void Sort(int[] array) { 
+                            
+        }
+    }
+}
