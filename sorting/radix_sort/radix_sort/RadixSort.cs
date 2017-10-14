13b263a6b656dd276f576e073e8a582b7d38b932
YI ZHANG <scaccterran@gmail.com>
2017-10-14 13:55:40 -0400

    radix sort

--- sorting/radix_sort/radix_sort/RadixSort.cs
+++ sorting/radix_sort/radix_sort/RadixSort.cs
@@ -10,8 +10,28 @@ namespace radix_sort {
          * sort an array in ascending order - this algorithm modifies the original array
          * @param {int[]} [array] - array to sort
          */
-        public void Sort(int[] array) { 
-                            
+        public void Sort(int[] array) {
+            //total digit places to sort
+            int places = array.Max().ToString().Length;
+            for(int i = 0, exponent = 1; i < places; exponent *= 10, i++) {
+                int[] counter = new int[array.Length];
+                Array.Clear(counter, 0, counter.Length);
+                //count digit occurrences
+                for(int j = 0; j < array.Length; j++) {
+                    counter[array[j] / exponent % 10]++;
+                }
+                for(int j = 1; j < array.Length; j++) {
+                    counter[j] += counter[j - 1];
+                }
+                //output array
+                int[] output = new int[array.Length];
+                for(int j = array.Length - 1; j >= 0; j--) {
+                    int digit = array[j] / exponent % 10;
+                    output[counter[digit] - 1] = array[j];
+                    counter[digit]--;
+                }
+                output.CopyTo(array, 0);
+            }
         }
     }
 }
