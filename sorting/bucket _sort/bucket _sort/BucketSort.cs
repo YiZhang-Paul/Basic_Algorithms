f721ae5c1ed6df1046035a3395d4e7220ac75a7a
YI ZHANG <scaccterran@gmail.com>
2017-10-14 18:12:45 -0400

    bucket sort

--- sorting/bucket _sort/bucket _sort/BucketSort.cs
+++ sorting/bucket _sort/bucket _sort/BucketSort.cs
@@ -8,10 +8,46 @@ namespace bucket__sort {
     class BucketSort {
         /*
          * sort an array in ascending order - this algorithm modifies the original array
-         * @param {int[]} [array] - array to sort
+         * @param {double[]} [array] - array to sort
          */
-        public void Sort(int[] array) { 
-                            
+        public void Sort(double[] array) { 
+            //bucket to hold all numbers
+            var bucket = new Dictionary<int, List<double>>();
+            for(int i = 0; i < array.Length; i++) {
+                int index = (int)Math.Truncate(array[i] * 10);
+                //put number into bucket
+                bucket[index] = bucket.ContainsKey(index) ? bucket[index] : new List<double>();
+                bucket[index].Add(array[i]);                    
+            }
+            //sort each bucket
+            foreach(var pair in bucket) {
+                InsertionSort(pair.Value);
+            }
+            //put sorted buckets back into a whole array
+            for(int i = 0, outputIndex = 0; i < 10; i++) {
+                if(bucket.ContainsKey(i)) {
+                    foreach(double number in bucket[i]) {
+                        array[outputIndex++] = number;
+                    }
+                }
+            }
+        }
+        /*
+         * sort an list using insertion sort
+         * @param {List<double>} [list] - list to sort
+         */
+        public void InsertionSort(List<double> list) {
+            for(int i = 1; i < list.Count; i++) {
+                int curIndex = i;
+                double curValue = list[curIndex];
+                //shift larger numbers to the right
+                while(curIndex > 0 && list[curIndex - 1] > curValue) {
+                    list[curIndex] = list[curIndex - 1];
+                    curIndex--;
+                }
+                //insert current inspecting number
+                list[curIndex] = curValue;
+            }
         }
     }
 }
--- sorting/bucket _sort/bucket _sort/Program.cs
+++ sorting/bucket _sort/bucket _sort/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace bucket__sort {
     class Program {
         static void Main(string[] args) {
-            int[] array = new int[] { 2, 14, 11, 16, 82, 4, 34, 51, 1, 4 };
+            double[] array = new double[] { 0.01d, 0.85d, 0.55d, 0.11d, 0.51d, 0.025d, 0.71d, 0.495d, 0.33d, 0.45d };
             BucketSort sorter = new BucketSort();
             sorter.Sort(array);
             Console.WriteLine(string.Join(", ", array)); 
