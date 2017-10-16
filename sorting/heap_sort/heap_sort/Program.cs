25bf0793ce58b055009ca1ebb9082a9933d65f05
YI ZHANG <scaccterran@gmail.com>
2017-10-16 15:21:54 -0400

    heap sort

---
+++ sorting/heap_sort/heap_sort/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heap_sort {
+    class Program {
+        static void Main(string[] args) {
+
+            int[] array = new int[] { 2, 14, 11, 16, 82, 4, 34, 51, 1, 4 };
+            HeapSort sorter = new HeapSort();
+            sorter.Sort(array);
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
