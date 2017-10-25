38e4aac71a5b9bc3a4e01b36c6491e23ea38729f
YI ZHANG <scaccterran@gmail.com>
2017-10-24 20:41:55 -0400

    sleep sort

---
+++ sorting/sleep_sort/sleep_sort/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sleep_sort {
+    class Program {
+        static void Main(string[] args) {
+
+            int[] array = new int[] { 2, 9, 4, 1, 4 };
+            SleepSort sorter = new SleepSort();
+            sorter.Sort(array);
+        }
+    }
+}
---
+++ sorting/sleep_sort/sleep_sort/SleepSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace sleep_sort {
+    class SleepSort {
+        /// <summary>
+        /// log a number to console after [number] amount of seconds
+        /// </summary>
+        /// <param name="data">number to log</param>
+        public static void LogNumber(Object data) {
+
+            Thread.Sleep((int)data * 1000);
+            Console.WriteLine(data);
+        }
+        /// <summary>
+        /// sort (log) an array in ascending order
+        /// </summary>
+        /// <param name="array"></param>
+        public void Sort(int[] array) { 
+        
+            foreach(int number in array) {
+
+                new Thread(LogNumber).Start(number);
+            }
+        }
+    }
+}
